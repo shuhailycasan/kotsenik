a4c31fb92c9e7c098140e5f6ee0361c37c356bae
shuhailycasan <shuhayly12@hmail.com>
2025-07-22 16:57:54 +0800

    Layouts

--- resources/views/about.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('title','Home Page')
-
-@section('content')
-<h1>Home PAge content Here</h1>
-
-@endsection
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,3 +1,9 @@
+@extends('layouts.app')
+
+@section('title','Home')
+
+@section('content')
 <div>
     <h1>Hello World</h1>
 </div>
+@endsection
